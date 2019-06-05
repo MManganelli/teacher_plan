c92a2010f4aec28c2e4d5b7062b1cd4d266db00b
Raghu Betina <raghu@firstdraft.com>
2019-06-05 21:29:56 +0000

    Direct association between users and improvement_plans

--- spec/models/improvement_plan_spec.rb
+++ spec/models/improvement_plan_spec.rb
@@ -4,6 +4,8 @@ RSpec.describe ImprovementPlan, type: :model do
   
     describe "Direct Associations" do
 
+    it { should belong_to(:user) }
+
     it { should have_many(:comments) }
 
     it { should have_many(:managers) }
