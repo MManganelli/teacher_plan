bff64d274fcabf923a8d408b4f2109d1739951d3
Raghu Betina <raghu@firstdraft.com>
2019-06-05 21:29:56 +0000

    Direct association between goals and actions

--- app/models/goal.rb
+++ app/models/goal.rb
@@ -1,6 +1,9 @@
 class Goal < ApplicationRecord
   # Direct associations
 
+  has_many   :actions,
+             :dependent => :destroy
+
   belongs_to :imprevement_plan,
              :class_name => "ImprovementPlan"
 
